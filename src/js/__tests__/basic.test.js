6a83618efa493ea06140c3372469561aa71e2cb0
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-10 01:34:32 +0300

    make full task,add test

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,14 @@
-import sum from '../basic';
+import Team from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
-
-  expect(result).toBe(6);
+describe('check work class Team', () => {
+  test('return text of types', () => {
+    const expected = 'BowmanMagicianSwordman';
+    const test1 = new Team();
+    let text = '';
+    for (const i of test1) {
+      text += i;
+    }
+    const received = text;
+    expect(received).toEqual(expected);
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,57 @@
-// TODO: write your code here
-import sum from './basic';
+export default class Team {
+  constructor() {
+    this.heroes = [
+      {
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Маг',
+        type: 'Magician',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Мечник',
+        type: 'Swordman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+    ];
+  }
 
-console.log('worked');
+  [Symbol.iterator]() {
+    let index = -1;
+    const team = this.heroes;
 
-console.log(sum([1, 2]));
+    return {
+      next() {
+        index += 1;
+        if (index < team.length) {
+          return {
+            value: team[index].type,
+            done: !(index in team),
+          };
+        }
+        return {
+          done: !(index in team),
+        };
+      },
+    };
+  }
+}
+
+const test1 = new Team();
+for (const i of test1) { console.log(i); }
+
+// return {
+//     next: () => ({ value: team[++index].type, done: !(index in team) }),
+//   };
